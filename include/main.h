e2e74cafbf82124be1e6eab3b3816e384a9aed76
yifeiwangG100 <80433409+shahuhuyifei@users.noreply.github.com>
2023-03-28 17:05:11 +1100

    player info updated

--- include/main.h
+++ include/main.h
@@ -12,17 +12,55 @@
 // Declaration of the RFID reader
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
+// Create a struct for all players to store their information
 #define NUM_SPOTS 9
 struct player
 {
   String board[NUM_SPOTS];
-  String detectUid;
-  String digUid;
-  String collectUid;
+  String detect_uid;
+  String dig_uid;
+  String collect_uid;
+  String player_A_uid;
+  String player_B_uid;
+  String player_C_uid;
+};
+// 3 * Chicken Flavour Dry Food
+// 5 * Canned Chicken
+// 6 * Freeze-dried Chicken
+// 6 * Chicken Flavour Creamy Treat
+// 6 * Canned Beef
+// 7 * Beef Flavour Dry Food
+// 7 * Seafood Flavour Dry Food
+// 8 * Canned Seafood
+// 6 * Cucumber
+
+struct board
+{
+  int Level[NUM_SPOTS];
 };
 
-player player_A = {{"63 3A 51 42", "73 32 5B 42", "E3 BD 5A 42", "D3 A9 51 42", "D3 AF 51 42", "23 1E 52 42", "73 87 52 42", "93 EB 52 42", "93 F0 52 42"}};
-player player_B = {{"F3 B8 5A 42", "33 3C 5A 42", "63 B9 59 42", "A3 37 59 42", "53 58 53 42", "93 D2 53 42", "B3 31 59 42", "63 B2 58 42", "43 32 58 42"}};
-player player_C = {{"E3 41 54 42", "E3 47 54 42", "83 C8 54 42", "C3 39 55 42", "A3 BC 55 42", "A3 C2 55 42", "C3 35 56 42", "93 B3 56 42", "93 B9 56 42"}};
+// Define a constant to identify the current player
+#define CURRENT_PLAYER "A"
+
+player player_A = {{"63 3A 51 42", "73 32 5B 42", "E3 BD 5A 42", "D3 A9 51 42", "D3 AF 51 42", "23 1E 52 42", "73 87 52 42", "93 EB 52 42", "93 F0 52 42"},
+                   "00 7B 29 4A",
+                   "10 7B 29 4A",
+                   "20 7B 29 4A",
+                   "",
+                   "30 7B 29 4A",
+                   "40 7B 29 4A"};
+player player_B = {{"F3 B8 5A 42", "33 3C 5A 42", "63 B9 59 42", "A3 37 59 42", "53 58 53 42", "93 D2 53 42", "B3 31 59 42", "63 B2 58 42", "43 32 58 42"},
+                   "F0 79 29 4A",
+                   "00 7A 29 4A",
+                   "10 7A 29 4A",
+                   "20 7A 29 4A",
+                   "",
+                   "30 7A 29 4A"};
+player player_C = {{"E3 41 54 42", "E3 47 54 42", "83 C8 54 42", "C3 39 55 42", "A3 BC 55 42", "A3 C2 55 42", "C3 35 56 42", "93 B3 56 42", "93 B9 56 42"},
+                   "40 7A 29 4A",
+                   "50 7A 29 4A",
+                   "60 7A 29 4A",
+                   "80 7A 29 4A",
+                   ""};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 
+player this_player;
+
 // Convert uid to a string
 String uidToString()
 {
@@ -14,6 +16,27 @@ String uidToString()
   return outputStringUid;
 }
 
+// Identify the player
+void identifyPlayer(String playerIdentifier)
+{
+  if (playerIdentifier = "A")
+  {
+    this_player = player_A;
+  }
+  else if (playerIdentifier = "B")
+  {
+    this_player = player_B;
+  }
+  else if (playerIdentifier = "C")
+  {
+    this_player = player_C;
+  }
+  else
+  {
+    Serial.println("Unknown player");
+  }
+}
+
 void setup()
 {
   Serial.begin(9600); // Initialize serial communications
@@ -21,6 +44,8 @@ void setup()
   SPI.begin();                       // Init SPI bus
   mfrc522.PCD_Init();                // Init MFRC522
   mfrc522.PCD_DumpVersionToSerial(); // Show details of PCD - MFRC522 Card Reader details
+
+  identifyPlayer(CURRENT_PLAYER); // Identify the player. Can be A, B or C.
 }
 
 void loop()
@@ -36,6 +61,22 @@ void loop()
   {
     return;
   }
-
+  // Detect
   Serial.println(uidToString());
+  if (uidToString() == this_player.detect_uid)
+  {
+    while (true)
+    {
+      // Look for new cards
+      if (!mfrc522.PICC_IsNewCardPresent())
+      {
+        continue;
+      }
+      // Select one of the cards
+      if (!mfrc522.PICC_ReadCardSerial())
+      {
+        continue;
+      }
+    }
+  }
 }
